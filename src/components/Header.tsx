e4087562a15841958095a46d34bc2e3d11998012
katteyes <janeth.imatosc@gmail.com>
2025-04-16 00:15:47 -0500

    Implement Header component with navigation links and integrate Google Fonts

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,20 @@
+import React from "react";
+
 function Header() {
+  const links = ["INICIO", "ACERCA DE", "REVISTAS", "CONTACTO"];
+
   return (
-    <header>
-      <nav>
-        <a href=""> Inicio </a>
-        <a href=""> Acerca de </a>
-        <a href=""> Revistas </a>
-        <a href=""> Contacto</a>
+    <header className="w-full justify-center">
+      <nav
+        className="text-xl text-black space-x-8 font-inter flex items-center"
+        style={{ fontWeight: 400 }}
+      >
+        {links.map((link, index) => (
+          <React.Fragment key={index}>
+            <a href="">{link}</a>
+            {index < 3 && <div className="bg-[#111c85] w-0.5 h-6"></div>}
+          </React.Fragment>
+        ))}
       </nav>
     </header>
   );
