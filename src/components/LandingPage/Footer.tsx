275dfe9650e3eff65a7a3be7e5b80e0a3f8f0423
Yadhira <cerverayadhira@gmail.com>
2025-06-10 23:49:47 -0500

    Corrección del header y footer

--- src/components/LandingPage/Footer.tsx
+++ src/components/LandingPage/Footer.tsx
@@ -4,24 +4,24 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="min-h-[5rem] bg-[#323c96] text-white py-5">
-      <div className="max-w-2xl mx-auto flex flex-col sm:flex-row justify-between gap-4 md:gap-20" >
+    <footer className="min-h-[3rem] bg-[#323c96] text-white py-2 md:py-5 px-5">
+      <div className="max-w-2xl mx-auto flex flex-col sm:flex-row justify-between gap-2 md:gap-20" >
         {/*col-1*/}
         <div className="flex flex-col items-center sm:items-start">
           <img
             src="/logoDestinosBlanco.png"
-            className="w-auto h-20 rounded-md"
+            className="w-auto h-15 sm:h-20 rounded-md"
             alt="Logo Destinos"
           />
-          <span className="mt-1 text-[1rem] sm:text-[0.9rem]">Explora. Inspírate. Descubre el mundo.</span>
+          <span className="mt-1 text-[0.8rem] sm:text-[1rem]">Explora. Inspírate. Descubre el mundo.</span>
         </div>
 
         {/*col-2*/}
-        <ul className="text-[1rem] font-semibold sm:font-bold space-y-2 sm:space-y-3 text-center sm:text-left text-[#fffdfd62] sm:text-white mt-1.5">
+        <ul className="text-[0.8rem] sm:text-[1rem] font-semibold sm:font-bold space-y-1 sm:space-y-3 text-center sm:text-left text-[#fffdfd62] sm:text-white mt-1.5">
           <li>©{currentYear} Destinos Turismo</li>
           <li>Todos los derechos reservados</li>
           <li>
-          <a href="/politicas-de-privacidad" className="mt-1 text-[1rem] sm:text-[0.9rem] hover:text-[#fffdfd62]">
+          <a href="/politicas-de-privacidad" className="mt-1 text-[0.8rem] sm:text-[1rem]  hover:text-[#fffdfd62]">
             Políticas de Privacidad
           </a>
           </li>
--- src/components/LandingPage/Header.tsx
+++ src/components/LandingPage/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
 
-  const Divider = () => <div className="h-3.5 md:h-5 w-0.5 bg-blue-800" aria-hidden="true" />;
+  const Divider = () => <div className="h-3.5 md:h-5 w-[0.4px] md:w-0.5 bg-blue-800" aria-hidden="true" />;
 
   const navigationItems = [
     { label: 'INICIO', href: '/' },
@@ -37,15 +37,15 @@ return (
   <div className="relative w-full flex items-center py-2">
 
   <div className="absolute left-1/2 top-5 transform -translate-x-1/2 -translate-y-1/2 flex items-center gap-3 mb-10">
-    <div className="h-18 w-22 md:h-30 md:w-35 lg:h-18 lg:w-40 flex items-center justify-center mr:2 md:mr-5  mt-15">
+    <div className="h-18 w-22 sm:h-22 sm:w-30 md:h-30 md:w-35 lg:h-18 lg:w-40 flex items-center justify-center mr:2 md:mr-5  mt-15">
       <img src="/logoPerú.png" alt="Logo Perú" className="h-full object-contain" />
     </div>
 
-    <a href="/" className="h-25 w-28 md:h-30 md:w-40 lg:h-40 lg:w-50 flex items-center justify-center">
+    <a href="/" className="h-24 w-28 sm:h-28 sm:w-35 md:h-30 md:w-40 lg:h-40 lg:w-50 flex items-center justify-center">
       <img src="/logoDestinos.png" alt="Logo destinos" className="h-full object-contain" />
     </a>
 
-    <div className="h-18 w-22 md:h-30 md:w-35 lg:h-33 lg:w-42 flex items-center justify-center ml:2 md:ml-5 mt-13">
+    <div className="h-18 w-22 sm:h-22 sm:w-30 md:h-30 md:w-35 lg:h-33 lg:w-42 flex items-center justify-center ml:2 md:ml-5 mt-13">
       <img src="/logoCuba.png" alt="Logo Cuba" className="h-full object-contain" />
     </div>
   </div>
@@ -53,7 +53,7 @@ return (
     <div className="ml-auto hidden xl:flex">
       <a
         href="/login"
-        className="flex text-sm items-center gap-1.5 px-4 py-2 hover:bg-[#f8f0e3]/30 bg-[#FFFFFF] border-[0.1rem] border-[#111C85] rounded-3xl transition-all duration-300 whitespace-nowrap "
+        className="flex text-sm items-center gap-1.5 px-4 py-1.5 hover:bg-[#f8f0e3]/30 bg-[#FFFFFF] border-[0.1rem] border-[#111C85] rounded-3xl transition-all duration-300 whitespace-nowrap "
       >
         <span className="text-[#000000] hover:text-[#111C85]">Iniciar sesión</span>
         <img
@@ -68,7 +68,7 @@ return (
 </nav>
 
 <nav
-  className="flex flex-row items-center justify-between gap-5 md:gap-8 top-15 md:top-20 lg:top-15 px-5 bg-transparent relative z-50 p-0 w-full "
+  className="flex flex-row items-center justify-between gap-5 md:gap-8 top-15 md:top-18 px-1 sm:px-5 bg-transparent relative z-50 p-0 w-full "
   aria-label="Secundaria"
 >
 
@@ -84,7 +84,7 @@ return (
       <li className="relative group">
         <a
           href={item.href}
-          className={`text-[9px] md:text-base lg:text-[18px] py-5 px-[3px] md:px-4 lg:px-6 rounded-sm transition-colors duration-300 ${
+          className={`text-[12px] sm:text-[16px] md:text-[18px] lg:text-[18px] px-1 sm:px-2 md:px-4 lg:px-6 rounded-sm transition-colors duration-300 ${
           window.location.pathname === item.href
           ? "bg-[#f8f0e3]/20 text-[#111C85]"
           : "text-[#000000] hover:bg-[#f8f0e3]/30 hover:text-[#111C85]"
